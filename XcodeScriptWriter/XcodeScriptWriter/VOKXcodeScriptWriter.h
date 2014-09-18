298521738f865b7d57403814be6b05704d421299
Ellen Shapiro <designatednerd@gmail.com>
2014-09-18 08:31:06 -0500

    Initial project creation from template.

---
+++ XcodeScriptWriter/XcodeScriptWriter/VOKXcodeScriptWriter.h
@@ -0,0 +1,13 @@
+//
+//  VOKXcodeScriptWriter.h
+//  VOKXcodeScriptWriter
+//
+//  Created by Ellen Shapiro (Vokal) on 9/18/14.
+//  Copyright (c) 2014 Vokal Interactive. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface VOKXcodeScriptWriter : NSObject
+
+@end
